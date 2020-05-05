2e5d5a9735b2f5867d87d2fc5c443977ec138bf5
River <20207075@qq.com>
2020-05-05 10:59:27 +0800

    init

---
+++ db/init/init.sql
@@ -0,0 +1,5 @@
+source /opt/guns/initdb.sql;
+use guns;
+source /opt/guns/guns.sql;
+use biz;
+source /opt/guns/biz.sql;
---
+++ db/sql/initdb.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE `guns` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;;
+use guns;
+CREATE DATABASE `biz` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+use biz;
